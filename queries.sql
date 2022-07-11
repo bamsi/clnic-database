11080e9135cb6b22e263ba6be168ae2fb772d8a7
bamsi <hibamsi@gmail.com>
2022-07-11 15:22:46 +0300

    update date range in select query

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 select * from animals where name like '%mon';
-select * from animals where date_of_birth BETWEEN '1-1-2016' AND '31-12-2019';
+select * from animals where date_of_birth BETWEEN '2016-1-1' AND '2019-12-31';
 select * from animals where neutered = true and escape_attempts < 3;
 select date_of_birth from animals where name = 'Agumon' OR name = 'Pikachu';
 select name, escape_attempts from animals where weight_kg > 10.5;
