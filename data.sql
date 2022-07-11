10889cd06671a8f5299bd34c1ffdada6890d4ed5
bamsi <hibamsi@gmail.com>
2022-07-11 14:48:50 +0300

    update date format

--- data.sql
+++ data.sql
@@ -1,8 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '3/2/2020',0, true, 10.23);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '15/11/2018',2, true, 8);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '7/1/2021',1, false, 15.04);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '12/5/2017',5, true, 11);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-2-3',0, true, 10.23);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15',2, true, 8);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-1-7',1, false, 15.04);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12',5, true, 11);
 
 
